a70251c360a97875ff97fbf1fb2d1c2b8d25578c
kareemshaaban221 <kareemshaaban221gmail.com>
2024-02-09 23:23:58 +0200

    make: base_model

--- models/base_model.py
+++ models/base_model.py
@@ -1,8 +1,35 @@
 #!/usr/bin/python3
 """ My class module
 """
+import uuid
+import datetime
+now = datetime.datetime.now
 
-def BaseModel():
+
+class BaseModel():
     """ My class
     """
-    pass
+
+    def __init__(self):
+        """_summary_
+        """
+        self.id = str(uuid.uuid4())
+        self.created_at = now()
+        self.updated_at = now()
+
+    def __str__(self):
+        """_summary_
+
+        Returns:
+            _type_: _description_
+        """
+        return "[{}] ({}) {}".format(
+            self.__class__.__name__,
+            self.id,
+            self.__dict__
+        )
+
+    def save(self):
+        """_summary_
+        """
+        self.updated_at = now()
