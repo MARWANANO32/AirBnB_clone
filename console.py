3ef0c8c83509dc891a49ead6de68665b4c5dc83c
kareemshaaban221 <kareemshaaban221@gmail.com>
2024-02-12 00:31:12 +0200

    test

--- console.py
+++ console.py
@@ -13,25 +13,8 @@ class HBNBCommand(cmd.Cmd):
         """EOF command to exit the program\n"""
         return True
 
-    def Create(self, args):
-        if args == "":
-            print("** class name missing **")
-        elif args not in HBNBCommand:
-            print("** class doesn't exist **")
-
-    def Show(self, args):
-        a = 0
-        arg = args.split()
-
-        if args == "":
-            print("** class name missing **")
-        elif arg[0] not in HBNBCommand:
-            print("** class doesn't exist **")
-        elif len(arg) < 2:
-            print("** instance id missing **")
-        else:
-            print("** no instance found **")
-
+    def emptyline(self):
+        return False
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
